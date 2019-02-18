531b58e571a31059bcb406e85d6907a875541686
zachary tyhacz <zactyh@gmail.com>
2019-02-17 20:24:29 -0500

    got to run

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-import token from './token.js';
+const token = require('./token.js');
 
 client.on('ready', () => {
- console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
  });
 
 client.on('message', msg => {
- if (msg.content === 'ping') {
- msg.reply('pong');
- }
+	if (msg.content === 'ping') {
+		msg.reply('pong');
+ 	}
  });
 
 client.login(token);
